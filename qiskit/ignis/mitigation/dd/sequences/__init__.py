03756456362e2ad04c5a3d5ccc66be770449cb6b
Adrien Suau <adrien.suau@cerfacs.fr>
2021-03-16 16:40:39 +0100

    Added CP and CPMG DD sequences to __init__.py

--- qiskit/ignis/mitigation/dd/sequences/__init__.py
+++ qiskit/ignis/mitigation/dd/sequences/__init__.py
@@ -13,3 +13,8 @@
 from .xy4 import XY4DynamicalDecouplingSequence, XY4PulseDynamicalDecouplingSequence
 from .kdd import XY4KDDDynamicalDecouplingSequence
 from .hahn import HahnSpinEchoDynamicalDecouplingSequence
+from .cp import CarrPurcellDynamicalDecouplingSequence
+from .cpmg import (
+    CPMGGateDynamicalDecouplingSequence,
+    CPMGPulseDynamicalDecouplingSequence,
+)
